40d19b48f5ca61b2f9d156c98a3e37f77c36b7af
Mamtha kumar <94455713+mamthaudaykumar@users.noreply.github.com>
2025-10-24 12:51:05 +0100

    Sg added

--- ec2-jenkins/main.tf
+++ ec2-jenkins/main.tf
@@ -7,10 +7,18 @@ module "networking" {
   cidr_private_subnet  = var.cidr_private_subnet
 }
 
+module "securitygroup" {
+  source              = "./modules/security-group"
+  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+  vpc_id              = module.networking.dev_proj_vpc_id
+  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+}
+
 module "ec2-jenkins" {
   source               = "./modules/ec2-jenkins"
   ec2_ami_id_jenkins             = var.ec2_ami_id_jenkins
   instance_type_jenkins             = var.instance_type_jenkins
   ec2_name_jenkins   = var.ec2_name_jenkins
   subnet_id     = module.networking.dev_proj_public_subnets[0]
+  security_group_id     = module.securitygroup.sg_ec2_sg_ssh_http_id
 }
--- ec2-jenkins/modules/ec2-jenkins/main.tf
+++ ec2-jenkins/modules/ec2-jenkins/main.tf
@@ -2,12 +2,16 @@ variable "ec2_ami_id_jenkins" {}
 variable "instance_type_jenkins" {}
 variable "ec2_name_jenkins" {}
 variable "subnet_id" {}
+variable "security_group_id" {
+  
+}
 
 
 resource "aws_instance" "dev-proj-ec2-jenkins" {
   ami           = var.ec2_ami_id_jenkins
   instance_type = var.instance_type_jenkins
   subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
   associate_public_ip_address = true
 
   tags = {
